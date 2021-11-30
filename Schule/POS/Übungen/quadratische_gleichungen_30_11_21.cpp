9322e10516bf2f74f0fdd2c5710eff02b6d2996b
Simon Löschke <simon-loeschke@posteo.de>
2021-11-30 21:17:31 +0100

    skript zu quadratischen Gleichungen

---
+++ Schule/POS/Übungen/quadratische_gleichungen_30_11_21.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+	double a,b,c;
+	double x1;
+	double x2;
+	printf("Geben Sie die Zahl a ein \n");
+	scanf("%lf",&a);
+	printf("Geben Sie die Zahl b ein \n");
+	scanf("%lf",&b);
+	printf("Geben Sie die Zahl c ein \n");
+	scanf("%lf",&c);
+	double w = b*b - 4*a*c;
+	printf("Wert w = %lf \n",w);
+	
+	if (w >= 0)
+	{
+		x1 = (-b + sqrt(w))/(2*a);
+		x2 = (-b - sqrt(w))/(2*a);
+		printf("X1 =  %lf \n X2 = %lf",x1,x2);
+	}
+	else 
+	{
+		printf("Es gibt keine Loesung!");
+	}
+	
+	return 0;
+}
