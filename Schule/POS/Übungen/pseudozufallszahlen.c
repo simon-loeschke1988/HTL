eed7f8187044f71e418a1272383ab1879567b1e8
simon löschke <simon-loeschke@posteo.de>
2022-01-03 13:45:47 +0100

    üben

--- Schule/POS/Übungen/pseudozufallszahlen.c
+++ Schule/POS/Übungen/pseudozufallszahlen.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+
 
 
 int main()
 {
+    int pzz;
+    //initialisiert die Zufallszahl
+    time_t t; 
+    time (&t);
+    srand((int) t);
+
+    // Zufallszahl generieren
     
+    
+    pzz = rand();
+
+    printf("Zufallszahl: %d\n",pzz);
+
     return 0;
 }
