796cccff36c5efc74c42cb5dc8f890f7c16891f1
simon löschke <simon-loeschke@posteo.de>
2022-02-03 11:16:41 +0100

    lernen

--- Schule/POS/Übungen/schleifen/schleifen.c
+++ Schule/POS/Übungen/schleifen/schleifen.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 //Übung 6.2.2
 void uebung622()
@@ -24,7 +25,7 @@ void uebung624()
 {
     short int b;
     printf("\n\nGeben Sie die Laenge der P.-Summe ein: \n");
-    scanf("%d", &b);
+    scanf("%hd", &b); //Compiler fordert %hd als Placeholder.
     short int i, a[b], c[b];
 
     a[0] = 1;
@@ -37,11 +38,64 @@ void uebung624()
         printf("\n\t a: %d \n\t b: %d\n", a[i], c[i]);
     };
 }
+// Hier Beginn Übung 6.2.5 -> Zwei Funktionen
+
+long long bin2dec(long long binaer)
+{
+    long long dezimalzahl = 0, i = 0, moduloperation;
+
+    while (binaer != 0)
+    {
+        moduloperation = binaer % 10;
+        binaer /= 10;
+        dezimalzahl += moduloperation * pow(2, i);
+        ++i;
+    }
+
+    return dezimalzahl;
+}
+void uebung625()
+{
+    long long binaer;
+    printf("Geben Sie eine Binärzahl ein:\n"); //Bash liest umlaute/Compiler kann damit umgehen.
+    scanf("%lld", &binaer);
+    printf("Die Dezimalzahl ist: %lld\n", bin2dec(binaer));
+}
+// Ende Übung 6.2.5
+
+//Übung 6.2.6
+
+long long dec2bin(unsigned int dzahl);
+void uebung626()
+{
+    unsigned int dezimal;
+    printf("Geben Sie eine Dezimalzahl ein: ");
+    scanf("%d", &dezimal);
+    printf("Die Zahl %d lautet %lld in ihrer binären Representation", dezimal, dec2bin(dezimal));
+}
+long long dec2bin(unsigned int dzhal)
+{
+    long long bzahl = 0;
+    int rem, i = 1;
+    int dzahl;
+
+    while (dzahl != 0)
+    {
+        rem = dzahl % 2;
+        dzahl /= 2;
+        bzahl += rem * i;
+        i *= 10;
+    }
+
+    return bzahl;
+}
 
 
 int main()
 {
     uebung622();
     uebung624();
+    uebung625();
+    uebung626();
     return 0;
 }
