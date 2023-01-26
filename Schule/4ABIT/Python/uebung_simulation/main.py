2352c5510f01d62763b6ea23865b8d38cba34f53
Simon Löschke <simon-loeschke@posteo.de>
2023-01-26 18:26:37 +0100

    Übung POS

--- Schule/4ABIT/Python/uebung_simulation/main.py
+++ Schule/4ABIT/Python/uebung_simulation/main.py
@@ -1,5 +1,25 @@
 # Simulieren Sie die lineare gleichförmige Bewegung einer Kugel (Kreis)
 # Wenn die Kugel am Rand des Fensters anstößt, soll sie sich wie eine Billiardkugel verhalten.
+# s(t) = v *t +s0
+
+import pygame
+import sys
+import pygame
+from underclasses import *
+from pygame import gfxdraw
+
+if __name__=='__main__':
+    
+    class window:
+        def __init__(self,sim):
+            self.sim = sim
+            self.offset =(0,0)
+            
+    
+
+
+
+
 
 
 
@@ -9,5 +29,5 @@
 
 
 if __name__== "__main__":
-    pass
+    
 
